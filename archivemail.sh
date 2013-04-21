e08c9cca73e57f39b1f17488aac5e08210a90841
Prasanna Santhanam <tsp@apache.org>
2013-04-21 11:01:27 +0530

    amzn for blog host
    credit is used a lot on xen-devel. beats me!
    some changes to archiving policies

--- archivemail.sh
+++ archivemail.sh
@@ -10,6 +10,7 @@ archivemail --delete --days=3 $HOME/mail/junk
 archivemail --delete --days=3 $HOME/mail/git  #available in git history
 archivemail --delete --days=3 $HOME/mail/cloudstack-git  #not required - check archives 
 archivemail --delete --days=3 $HOME/mail/cloudstack-bugs  #not required - check archives
+archivemail --delete --days=3 $HOME/mail/xen  #not required - check archives
 archivemail --delete --days=1 $HOME/mail/hudson  
 archivemail --delete --days=1 $HOME/mail/infra
 archivemail --delete --days=1 $HOME/mail/cloudstack-jenkins  
